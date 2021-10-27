d78f0a953c350ba72898179f460d93b7f2c59294
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2021-10-27 16:54:02 +0500

    get completed navbar menu

---
+++ js/mainjs.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iOSi|Pad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add("_touch");
+} else {
+  document.body.classList.add("pc");
+}
